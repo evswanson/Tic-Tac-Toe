58c21abe8d9826754eebdb6bf73e9d0ec9efcce4
Evan Swanson <47950462+evswanson@users.noreply.github.com>
2020-06-22 14:52:41 -0500

    Uploaded Code

---
+++ src/main/kotlin/edu/uiowa/project2820.kt
@@ -0,0 +1,10 @@
+package edu.uiowa
+
+// this is where you will begin the first version of your project
+// but you will also need to connect this project with your github.uiowa.edu
+// repository, as explained in the discussion section 9th November
+
+fun main(args: Array<String>) {
+    // you can do some testing here, though unit testing needs to be
+    // in the src/test/java directory
+}
